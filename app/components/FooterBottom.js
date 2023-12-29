8f31761a88c58675695aebb757724301c55a597b
Your Name <okhuomonajayi54@gmail.com>
2023-12-29 21:03:22 +0100

    fix

--- app/components/FooterBottom.js
+++ app/components/FooterBottom.js
@@ -4,7 +4,7 @@ export default function FooterBottom() {
   return (
     <div className="bg-[#f4f4f4]">
       <div className="max-w-[1440px] mx-auto">
-        <div className="flex justify-between w-full items-center py-12 border-t-[1px] border-b-[1px] border-gray-300 mb-20">
+        <div className="flex justify-between w-full items-center py-12 border-t-[1px] border-b-[1px] border-gray-300 ">
           <h6 className="text-sm silka">
             Polarium Energy Solutions AB <br /> Registration no. 556986-5461
           </h6>
@@ -12,6 +12,9 @@ export default function FooterBottom() {
             © 2023 Polarium Energy Solutions AB – All Rights Reserved <br />{" "}
             Privacy & cookie policy
           </h6>
+        </div>
+        <div className='pt-20'>
+
         </div>
       </div>
     </div>
